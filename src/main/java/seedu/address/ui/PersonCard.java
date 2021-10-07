0635813897903301566e37caadc87412d791c031
Daniel Sim <dsrecompassvale@gmail.com>
2021-10-07 22:52:14 +0800

    Make UI clearer for leaves

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -54,7 +54,7 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
-        leaves.setText(person.getLeaves().toString());
+        leaves.setText("Leaves: " + person.getLeaves().toString());
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
