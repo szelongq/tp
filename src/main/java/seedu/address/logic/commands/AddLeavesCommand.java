8d9779416e60cf971d1a3700e6d52e1c70ad31d3
Daniel Sim <dsrecompassvale@gmail.com>
2021-10-07 22:50:12 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/commands/AddLeavesCommand.java
+++ src/main/java/seedu/address/logic/commands/AddLeavesCommand.java
@@ -3,14 +3,14 @@ package seedu.address.logic.commands;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Leaves;
 import seedu.address.model.person.Person;
-
-import java.util.List;
 
 /**
  * Adds some number of leaves to an employee in HeRon.
