4446b5749c30f0a37f43ad62222a8ea41bfbf82b
Wee Han <weehanng@yahoo.com>
2021-10-08 18:04:27 +0800

    fix checkstyle errors

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -7,9 +7,9 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,11 +19,11 @@ import seedu.address.logic.commands.DeleteCommand;
 //import seedu.address.logic.commands.EditCommand;
 //import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
+//import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+//import seedu.address.model.person.NameContainsKeywordsPredicate;
 //import seedu.address.model.person.Person;
 //import seedu.address.testutil.EditPersonDescriptorBuilder;
 //import seedu.address.testutil.PersonBuilder;
