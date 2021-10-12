e4134013c60fbfbad78e00d9f89c858331accce9
1waykiat <1waykiat@gmail.com>
2021-10-12 09:49:27 +0800

    edit comment for regex

--- src/main/java/seedu/address/model/person/Role.java
+++ src/main/java/seedu/address/model/person/Role.java
@@ -12,7 +12,7 @@ public class Role {
     public static final String MESSAGE_CONSTRAINTS = "Role can take any values, and it should not be blank";
 
     /*
-     * The first character of the address must not be a whitespace,
+     * The first character of the role must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
     public static final String VALIDATION_REGEX = "[^\\s].*";
