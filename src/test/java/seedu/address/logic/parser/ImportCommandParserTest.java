43a0ce2cc0d5b43b11711e04ec85e1518c061ba2
boonhaii <boonzxc@gmail.com>
2021-10-23 13:34:03 +0800

    Update naming of tests in ImportCommandParserTest

--- src/test/java/seedu/address/logic/parser/ImportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ImportCommandParserTest.java
@@ -16,7 +16,7 @@ public class ImportCommandParserTest {
 
     // Displays error message if no file path is specified.
     @Test
-    public void missingFilePath_failure() {
+    public void parse_missingFilePath_failure() {
         String input = ImportCommand.COMMAND_WORD;
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportCommand.MESSAGE_USAGE);
         assertParseFailure(parser, input, expectedMessage);
@@ -24,7 +24,7 @@ public class ImportCommandParserTest {
 
     // Able to read filepath with directories utilising space character.
     @Test
-    public void filepathWithSpace_success() {
+    public void parse_filepathWithSpace_success() {
         String input = ImportCommand.COMMAND_WORD + " " + TEST_FILEPATH_WITH_SPACE;
         try {
             ImportCommand command = parser.parse(input);
