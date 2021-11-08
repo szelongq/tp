9d4d3e1ffc79f8d6e280ad8be9e056198a948fcb
boonhaii <boonzxc@gmail.com>
2021-11-08 15:55:28 +0800

    Fix Checkstyle errors

--- src/test/java/seedu/address/logic/parser/AssignLeaveCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AssignLeaveCommandParserTest.java
@@ -1,10 +1,5 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.AssignLeaveCommand;
-
-import java.time.LocalDate;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_DATE_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
@@ -12,32 +7,45 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailur
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
-public class AssignLeaveCommandParserTest {
-    private AssignLeaveCommandParser parser = new AssignLeaveCommandParser();
+import java.time.LocalDate;
 
-    private static final String VALID_TYPICAL_DATE = "2020-02-20";
-    private static final String VALID_LEAP_YEAR_DATE = "2020-02-29";
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.AssignLeaveCommand;
+
+public class AssignLeaveCommandParserTest {
+
+    private static AssignLeaveCommandParser parser = new AssignLeaveCommandParser();
+
+    private static final String FIRST_INDEX = "1";
+
     private static final String INVALID_LEAP_YEAR_DATE = "2021-02-29"; // 2021 is not a leap year.
     private static final String INVALID_MONTH_DATE = "2020-50-29"; // 50 is not a valid month for a date
     private static final String INVALID_DAY_DATE = "2020-02-50"; // 50 is not a valid day for a date
-    private static final String INVALID_YEAR_DATE = "999-02-50"; // Year must be in 4 digits
-    private static final String FIRST_INDEX = "1";
+    private static final String INVALID_THREE_DIGIT_YEAR_DATE = "999-02-50"; // Year must be in 4 digits
+    private static final String INVALID_FIVE_DIGIT_YEAR_DATE = "20202-02-50"; // Year must be in 4 digits
 
+    private static final String VALID_TYPICAL_DATE = "2020-02-20";
+    private static final String VALID_LEAP_YEAR_DATE = "2020-02-29";
+
+    private static final String INPUT_DATE_NOT_GIVEN = " " + FIRST_INDEX + " " + PREFIX_DATE;
     private static final String INPUT_INDEX_NOT_GIVEN = " " + PREFIX_DATE + VALID_TYPICAL_DATE;
-    private static final String INPUT_DATE_NOT_GIVEN =  " " + FIRST_INDEX + " " + PREFIX_DATE;
+
+    private static final String INPUT_INVALID_DAY = " " + FIRST_INDEX + " " + PREFIX_DATE + INVALID_DAY_DATE;
+    private static final String INPUT_INVALID_LEAP_YEAR = " " + FIRST_INDEX + " " + PREFIX_DATE
+            + INVALID_LEAP_YEAR_DATE;
+    private static final String INPUT_INVALID_MONTH = " " + FIRST_INDEX + " " + PREFIX_DATE + INVALID_MONTH_DATE;
+    private static final String INPUT_INVALID_THREE_DIGIT_YEAR = " " + FIRST_INDEX + " " + PREFIX_DATE
+            + INVALID_THREE_DIGIT_YEAR_DATE;
+    private static final String INPUT_INVALID_FIVE_DIGIT_YEAR = " " + FIRST_INDEX + " " + PREFIX_DATE
+            + INVALID_FIVE_DIGIT_YEAR_DATE;
 
     private static final String INPUT_VALID_LEAP_YEAR = " " + FIRST_INDEX + " " + PREFIX_DATE + VALID_LEAP_YEAR_DATE;
     private static final String INPUT_VALID_TYPICAL_DATE = " " + FIRST_INDEX + " " + PREFIX_DATE + VALID_TYPICAL_DATE;
 
-    private static final String INPUT_INVALID_LEAP_YEAR = " " + FIRST_INDEX + " " + PREFIX_DATE
-            + INVALID_LEAP_YEAR_DATE;
-    private static final String INPUT_INVALID_MONTH = " " + FIRST_INDEX + " " + PREFIX_DATE + INVALID_MONTH_DATE;
-    private static final String INPUT_INVALID_DAY = " " + FIRST_INDEX + " " + PREFIX_DATE + INVALID_DAY_DATE;
-    private static final String INPUT_INVALID_YEAR = " " + FIRST_INDEX + " " + PREFIX_DATE + INVALID_YEAR_DATE;
-
     // Missing Index -> Failure
     @Test
-    public void parse_indexNotGiven_InvalidCommandError() {
+    public void parse_indexNotGiven_invalidCommandError() {
         String input = INPUT_INDEX_NOT_GIVEN;
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AssignLeaveCommand.MESSAGE_USAGE);
         assertParseFailure(parser, input, expectedMessage);
@@ -45,7 +53,7 @@ public class AssignLeaveCommandParserTest {
 
     // Missing Date -> Failure
     @Test
-    public void parse_dateNotGiven_InvalidCommandError() {
+    public void parse_dateNotGiven_invalidCommandError() {
         String input = INPUT_DATE_NOT_GIVEN;
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AssignLeaveCommand.MESSAGE_USAGE);
         assertParseFailure(parser, input, expectedMessage);
@@ -53,7 +61,7 @@ public class AssignLeaveCommandParserTest {
 
     // Invalid Month -> Failure
     @Test
-    public void parse_givenDateMonthNotValid_InvalidDateError() {
+    public void parse_givenDateMonthNotValid_invalidDateError() {
         String input = INPUT_INVALID_MONTH;
         String expectedMessage = String.format(MESSAGE_INVALID_DATE_FORMAT, AssignLeaveCommand.MESSAGE_USAGE);
         assertParseFailure(parser, input, expectedMessage);
@@ -61,7 +69,7 @@ public class AssignLeaveCommandParserTest {
 
     // Invalid Day -> Failure
     @Test
-    public void parse_givenDateDayNotValid_InvalidDateError() {
+    public void parse_givenDateDayNotValid_invalidDateError() {
         String input = INPUT_INVALID_DAY;
         String expectedMessage = String.format(MESSAGE_INVALID_DATE_FORMAT, AssignLeaveCommand.MESSAGE_USAGE);
         assertParseFailure(parser, input, expectedMessage);
@@ -77,8 +85,16 @@ public class AssignLeaveCommandParserTest {
 
     // Invalid Year -> Failure
     @Test
-    public void parse_givenDateYearNotValid_InvalidDateError() {
-        String input = INPUT_INVALID_YEAR;
+    public void parse_givenDateThreeDigitYearNotValid_invalidDateError() {
+        String input = INPUT_INVALID_THREE_DIGIT_YEAR;
+        String expectedMessage = String.format(MESSAGE_INVALID_DATE_FORMAT, AssignLeaveCommand.MESSAGE_USAGE);
+        assertParseFailure(parser, input, expectedMessage);
+    }
+
+    // Invalid Year -> Failure
+    @Test
+    public void parse_givenDateFiveDigitYearNotValid_invalidDateError() {
+        String input = INPUT_INVALID_FIVE_DIGIT_YEAR;
         String expectedMessage = String.format(MESSAGE_INVALID_DATE_FORMAT, AssignLeaveCommand.MESSAGE_USAGE);
         assertParseFailure(parser, input, expectedMessage);
     }
--- src/test/java/seedu/address/logic/parser/RemoveLeavesBeforeCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemoveLeavesBeforeCommandParserTest.java
@@ -1,40 +1,41 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.RemoveLeavesBeforeCommand;
-
-import java.time.LocalDate;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_DATE_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.RemoveLeavesBeforeCommand;
 
 public class RemoveLeavesBeforeCommandParserTest {
-    private RemoveLeavesBeforeCommandParser parser = new RemoveLeavesBeforeCommandParser();
 
-    private static final String VALID_TYPICAL_DATE = "2020-02-20";
-    private static final String VALID_LEAP_YEAR_DATE = "2020-02-29";
+    private static RemoveLeavesBeforeCommandParser parser = new RemoveLeavesBeforeCommandParser();
+
     private static final String INVALID_LEAP_YEAR_DATE = "2021-02-29"; // 2021 is not a leap year.
     private static final String INVALID_MONTH_DATE = "2020-50-29"; // 50 is not a valid month for a date
     private static final String INVALID_DAY_DATE = "2020-02-50"; // 50 is not a valid day for a date
-    private static final String INVALID_YEAR_DATE = "999-02-50"; // Year must be in 4 digits
+    private static final String INVALID_THREE_DIGIT_YEAR_DATE = "999-02-50"; // Year must be in 4 digits
+    private static final String INVALID_FIVE_DIGIT_YEAR_DATE = "20202-02-50"; // Year must be in 4 digits
+    private static final String VALID_TYPICAL_DATE = "2020-02-20";
+    private static final String VALID_LEAP_YEAR_DATE = "2020-02-29";
 
-    private static final String INPUT_DATE_NOT_GIVEN =  " " + PREFIX_DATE;
-
-    private static final String INPUT_VALID_LEAP_YEAR = " " + PREFIX_DATE + VALID_LEAP_YEAR_DATE;
-    private static final String INPUT_VALID_TYPICAL_DATE = " " + PREFIX_DATE + VALID_TYPICAL_DATE;
-
+    private static final String INPUT_DATE_NOT_GIVEN = " " + PREFIX_DATE;
     private static final String INPUT_INVALID_LEAP_YEAR = " " + PREFIX_DATE + INVALID_LEAP_YEAR_DATE;
     private static final String INPUT_INVALID_MONTH = " " + PREFIX_DATE + INVALID_MONTH_DATE;
     private static final String INPUT_INVALID_DAY = " " + PREFIX_DATE + INVALID_DAY_DATE;
-    private static final String INPUT_INVALID_YEAR = " " + PREFIX_DATE + INVALID_YEAR_DATE;
+    private static final String INPUT_INVALID_THREE_DIGIT_YEAR = " " + PREFIX_DATE + INVALID_THREE_DIGIT_YEAR_DATE;
+    private static final String INPUT_INVALID_FIVE_DIGIT_YEAR = " " + PREFIX_DATE + INVALID_FIVE_DIGIT_YEAR_DATE;
+    private static final String INPUT_VALID_LEAP_YEAR = " " + PREFIX_DATE + VALID_LEAP_YEAR_DATE;
+    private static final String INPUT_VALID_TYPICAL_DATE = " " + PREFIX_DATE + VALID_TYPICAL_DATE;
 
     // Missing Date -> Failure
     @Test
-    public void parse_dateNotGiven_InvalidCommandError() {
+    public void parse_dateNotGiven_invalidCommandError() {
         String input = INPUT_DATE_NOT_GIVEN;
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemoveLeavesBeforeCommand.MESSAGE_USAGE);
         assertParseFailure(parser, input, expectedMessage);
@@ -42,7 +43,7 @@ public class RemoveLeavesBeforeCommandParserTest {
 
     // Invalid Month -> Failure
     @Test
-    public void parse_givenDateMonthNotValid_InvalidDateError() {
+    public void parse_givenDateMonthNotValid_invalidDateError() {
         String input = INPUT_INVALID_MONTH;
         String expectedMessage = String.format(MESSAGE_INVALID_DATE_FORMAT, RemoveLeavesBeforeCommand.MESSAGE_USAGE);
         assertParseFailure(parser, input, expectedMessage);
@@ -50,7 +51,7 @@ public class RemoveLeavesBeforeCommandParserTest {
 
     // Invalid Day -> Failure
     @Test
-    public void parse_givenDateDayNotValid_InvalidDateError() {
+    public void parse_givenDateDayNotValid_invalidDateError() {
         String input = INPUT_INVALID_DAY;
         String expectedMessage = String.format(MESSAGE_INVALID_DATE_FORMAT, RemoveLeavesBeforeCommand.MESSAGE_USAGE);
         assertParseFailure(parser, input, expectedMessage);
@@ -66,8 +67,16 @@ public class RemoveLeavesBeforeCommandParserTest {
 
     // Invalid Year -> Failure
     @Test
-    public void parse_givenDateYearNotValid_InvalidDateError() {
-        String input = INPUT_INVALID_YEAR;
+    public void parse_givenDateThreeDigitYearNotValid_invalidDateError() {
+        String input = INPUT_INVALID_THREE_DIGIT_YEAR;
+        String expectedMessage = String.format(MESSAGE_INVALID_DATE_FORMAT, RemoveLeavesBeforeCommand.MESSAGE_USAGE);
+        assertParseFailure(parser, input, expectedMessage);
+    }
+
+    // Invalid Year -> Failure
+    @Test
+    public void parse_givenDateFiveDigitYearNotValid_invalidDateError() {
+        String input = INPUT_INVALID_FIVE_DIGIT_YEAR;
         String expectedMessage = String.format(MESSAGE_INVALID_DATE_FORMAT, RemoveLeavesBeforeCommand.MESSAGE_USAGE);
         assertParseFailure(parser, input, expectedMessage);
     }
